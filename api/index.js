e270abe1f1414b3c16754307a74e093ba6b2936f
BoWen Liu <bowenlu6995@gmail.com>
2024-01-03 11:49:32 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt=bcrypt.genSaltSync(10);
 const secret ="wgaheshshsehs"
 
-app.use(cors({credentials:true,origin:'link-start-pop-culture-site-dhzc74j4t-bowen0225.vercel.app'}));
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname +'/uploads' ));
